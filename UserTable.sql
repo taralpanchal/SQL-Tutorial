59e2574f27fcfe3c9c4f05916996fb3fffbe3516
Taral Panchal <taral.facile@outlook.com>
2021-03-16 17:55:18 +0530

    perform like operator

--- UserTable.sql
+++ UserTable.sql
@@ -1,13 +1,14 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
-Insert into [FirstDB].[dbo].[user] (user_id,user_name,address,dob) 
-values 
-('1','Mosh','Ahmedabad',5-12-1995),
-('2','SS','Ahmedabad',5-12-1996),
-('3','Hardip','Ahmedabad',5-12-1995),
-('4','Rajesh','Surat',2-10-1997),
-('5','Sam','Rajkot',7-1-1999),
-('6','Chirag','Ahmedabad',8-82-2000)
 
 select * from [FirstDB].[dbo].[user]
 
-Select * from [FirstDB].[dbo].[user] where user_id = 1
+
+
+--update [FirstDB].[dbo].[user] set salary = '18000' where user_id = '6';
+--Select * from [FirstDB].[dbo].[user] where dob BETWEEN '1996-1-1' AND '2000-1-1' 
+ select * from [FirstDB].[dbo].[user] 
+ where user_name like '%h'
+  select * from [FirstDB].[dbo].[user] 
+ where user_name like '%a%'
+   select * from [FirstDB].[dbo].[user] 
+ where user_name like '_____h'
