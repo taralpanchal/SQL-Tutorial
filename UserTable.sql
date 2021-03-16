1ce028e59293d8c02b168162d2ac666773feec60
Taral Panchal <taral.facile@outlook.com>
2021-03-16 19:23:42 +0530

    Inner Join

--- UserTable.sql
+++ UserTable.sql
@@ -1,14 +1,15 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
 
-select * from [FirstDB].[dbo].[user]
+--select * from [FirstDB].[dbo].[product]
 
 
 
---update [FirstDB].[dbo].[user] set salary = '18000' where user_id = '6';
+--update [FirstDB].[dbo].[product] set user_id = '4' where user_name='Iphone';
 --Select * from [FirstDB].[dbo].[user] where dob BETWEEN '1996-1-1' AND '2000-1-1' 
- select * from [FirstDB].[dbo].[user] 
- where user_name like '%h'
-  select * from [FirstDB].[dbo].[user] 
- where user_name like '%a%'
-   select * from [FirstDB].[dbo].[user] 
- where user_name like '_____h'
+ --select * from [FirstDB].[dbo].[user]
+ --order by user_name DESC
+
+select address,dob,salary,name 
+from [FirstDB].[dbo].[user] 
+join [FirstDB].[dbo].[product]
+on [FirstDB].[dbo].[user].user_id = [FirstDB].[dbo].[product].user_id
