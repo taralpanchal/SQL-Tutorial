aaa70ae51151de2f5953e2300acb65c5b813d44c
Taral Panchal <taral.facile@outlook.com>
2021-03-17 16:26:36 +0530

    Copy table

--- self join.sql
+++ self join.sql
@@ -6,12 +6,8 @@
 --insert into [FirstDB].[dbo].[order] (order_id,status) values ('2','Processed')
 
 
---select * from [FirstDB].[dbo].[order],[FirstDB].[dbo].[user]
 
-select user_name from [FirstDB].[dbo].[user]
-Union 
-select user_name from [FirstDB].[dbo].[product] 
 
---select * into [FirstDB].[dbo].[copy] from [FirstDB].[dbo].[user]
+select * into [FirstDB].[dbo].[copy] from [FirstDB].[dbo].[user]
 
---select * from [FirstDB].[dbo].[copy]
+select * from [FirstDB].[dbo].[copy]
