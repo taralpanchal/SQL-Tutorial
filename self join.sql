07b9d2fced73e4515ed63190c695756ae95fc8b6
Taral Panchal <taral.facile@outlook.com>
2021-03-17 12:23:05 +0530

    Revert "Self join"
    This reverts commit 98f5f231790d461fb2ba0000e66d1e219c3484c8.

--- self join.sql
+++
@@ -1,9 +0,0 @@
---select * from [FirstDB].[dbo].[order]
-
---insert into [FirstDB].[dbo].[order] (order_id,status) values ('2','Processed')
-select [FirstDB].[dbo].[product].name, [FirstDB].[dbo].[user].user_name, [FirstDB].[dbo].[order].status
-from [FirstDB].[dbo].[product]
-Join [FirstDB].[dbo].[user]
-on [FirstDB].[dbo].[product].product_id = [FirstDB].[dbo].[user].user_id
-join [FirstDB].[dbo].[order]
-on [FirstDB].[dbo].[order].order_id = [FirstDB].[dbo].[product].product_id
