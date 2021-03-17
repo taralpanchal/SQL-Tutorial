9389b8b157192cd6fd85a7326fd2ccc369f91a80
Taral Panchal <taral.facile@outlook.com>
2021-03-17 16:24:56 +0530

    union

--- self join.sql
+++ self join.sql
@@ -1,17 +1,16 @@
-select * from [FirstDB].[dbo].[order]
-select * from [FirstDB].[dbo].[user]
-select * from [FirstDB].[dbo].[product]
+--select * from [FirstDB].[dbo].[order]
+--select * from [FirstDB].[dbo].[user]
+--select * from [FirstDB].[dbo].[product]
 
 
 --insert into [FirstDB].[dbo].[order] (order_id,status) values ('2','Processed')
 
-select * from [FirstDB].[dbo].[order] Cross join [FirstDB].[dbo].[user] 
 
 --select * from [FirstDB].[dbo].[order],[FirstDB].[dbo].[user]
 
---select user_name from [FirstDB].[dbo].[user]
---Union 
---select user_name from [FirstDB].[dbo].[product] 
+select user_name from [FirstDB].[dbo].[user]
+Union 
+select user_name from [FirstDB].[dbo].[product] 
 
 --select * into [FirstDB].[dbo].[copy] from [FirstDB].[dbo].[user]
 
